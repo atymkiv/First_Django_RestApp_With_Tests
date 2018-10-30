c7feeeb3f14a2a4a72e7e0f3e743aa48e4c476cc
atyma <atymkiv99@gmail.com>
2018-10-30 14:12:03 +0200

    deleted not rest functions from project

--- Table/urls.py
+++ Table/urls.py
@@ -6,9 +6,7 @@ from .views import RESTStudentDetail
 from . import views
 app_name = 'Table'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:student_id>/', views.students, name='students'),
-    path('rest-api/student-list/', RESTStudentList.as_view(), name ='rest_student_list'),
-    
+    url(r'^api-auth/', include('rest_framework.urls')),
+    path('rest-api/student-list/', RESTStudentList.as_view(), name ='rest_student_list'), 
     url(r'^rest-api/student-detail/(?P<pk>\d+)/$', RESTStudentDetail.as_view(), name="rest_student_detail"),
 ]
--- Table/views.py
+++ Table/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render,get_object_or_404
-
-from django.template import loader
-
 from .models import Student
 from rest_framework import generics
 from rest_framework.decorators import api_view
@@ -20,14 +16,3 @@ class RESTStudentDetail(generics.RetrieveUpdateDestroyAPIView):
 	serializer_class = StudentSerializer
 
 
-def index(request):
-    student_list = Student.objects.order_by('-rate')
-    template = loader.get_template('table/index.html')	
-    context = {'student_list' : student_list}
-    return render(request,'table/index.html',context)
-
-def students(request, student_id):
-	student = get_object_or_404(Student, pk=student_id)
-	return render(request, 'table/detail.html', {'student': student})
-
-# Create your views here.
