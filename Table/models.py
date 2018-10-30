4fadea963ee992e27b6d093ea8c0467f4a6fc479
atyma <atymkiv99@gmail.com>
2018-10-30 15:43:44 +0200

    started unit tests

--- Table/models.py
+++ Table/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Student(models.Model):
 	firstName = models.CharField(max_length = 30)
 	midName =models.CharField(max_length=30)
 	lastName = models.CharField(max_length=30)
-	rate = models.FloatField()
+	rate = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],)
 	def __str__(self):
 		return self.lastName
 # Create your models here.
