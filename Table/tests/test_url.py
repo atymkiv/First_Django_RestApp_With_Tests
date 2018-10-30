eb72a4a0586c733b188fa41f8fc08b7596083e1c
atyma <atymkiv99@gmail.com>
2018-10-30 23:40:39 +0200

    added pytest test for model

---
+++ Table/tests/test_url.py
@@ -0,0 +1,8 @@
+"""from django.urls import reverse, resolve, path
+
+class TestUrls:
+
+	def test_detail_url(self):
+	 	path = reverse('rest_student_detail', kwargs={'pk':1})
+	 	assert resolve(path).firstName =='Андрій'
+	"""
